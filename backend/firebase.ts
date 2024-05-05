e69c8d99ec8b78ffb4304d1cc1002405965eb4d7
achen09 <147263993+achen09@users.noreply.github.com>
2024-05-04 20:27:52 -0400

    fix path fb

--- backend/firebase.ts
+++ backend/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 
-const serviceAccount = require("./.firebase/service_account.json")
+const serviceAccount = require("./firebase/service_account.json")
 const app = initializeApp({
   credential: cert(serviceAccount),
 });
